1e73da5158b3f6371d39d6e3e7112581e9d4ba20
Nguyen Ninh Dao <nguyen.dao@epfl.ch>
2026-02-26 19:13:11 +0100

    feat: account service update followers count correctly

--- server/account-service/src/main/kotlin/org/ninh/instaclone/repository/UserDetailsRepository.kt
+++ server/account-service/src/main/kotlin/org/ninh/instaclone/repository/UserDetailsRepository.kt
@@ -1,10 +1,44 @@
 package org.ninh.instaclone.repository
 
+import jakarta.transaction.Transactional
 import org.ninh.instaclone.model.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import java.util.Optional
 
 interface UserDetailsRepository : JpaRepository<User, String> {
     fun findByUsername(username: String): Optional<User>
     fun existsByUsername(username: String): Boolean
+    @Modifying
+    @Transactional
+    @Query("""
+    UPDATE "user_account"
+    SET 
+        followers_count = 
+            CASE 
+                WHEN :counterType = 'FOLLOWERS' AND :action = 'INCREMENT' 
+                THEN followers_count + 1
+                WHEN :counterType = 'FOLLOWERS' AND :action = 'DECREMENT' AND followers_count > 0
+                THEN followers_count - 1
+                ELSE followers_count
+            END,
+        following_count = 
+            CASE 
+                WHEN :counterType = 'FOLLOWING' AND :action = 'INCREMENT'
+                THEN following_count + 1
+                WHEN :counterType = 'FOLLOWING' AND :action = 'DECREMENT' AND following_count > 0
+                THEN following_count - 1
+                ELSE following_count
+            END
+    WHERE username = :username
+""",
+        nativeQuery = true
+    )
+    fun updateFollowCounter(
+        @Param("username") username: String,
+        @Param("counterType") counterType: String,
+        @Param("action") action: String
+    ): Int
 }
