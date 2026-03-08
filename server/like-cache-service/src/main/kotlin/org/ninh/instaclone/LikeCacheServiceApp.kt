f6159d79d98a661693295c07d8024b30625c132b
Nguyen Ninh Dao <nguyen.dao@epfl.ch>
2026-03-08 22:02:30 +0100

    chore: set up like cache

---
+++ server/like-cache-service/src/main/kotlin/org/ninh/instaclone/LikeCacheServiceApp.kt
@@ -0,0 +1,10 @@
+package org.ninh.instaclone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LikeCacheServiceApp
+fun main(args: Array<String>) {
+    runApplication<LikeCacheServiceApp>(*args)
+}
